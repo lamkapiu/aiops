bbc96b3a3e56ff42470ed9f2d279d1baedfc9df4
linjiabiao <linjb@fxtcn.com>
2024-09-25 11:10:56 +0800

    add module3 zuoye

---
+++ module3/zuoye1/docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg -y
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg --yes
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin golang-go jq -y
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+# install nginx ingress
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+helm upgrade --install ingress-nginx ingress-nginx \
+  --repo https://kubernetes.github.io/ingress-nginx \
+  --namespace ingress-nginx --create-namespace
